95af184e18bce9ff2e916b15cf250bf05ccbf7a1
shinohara-san <someiyoshino0809@gmail.com>
2020-03-28 17:34:19 +0900

    20200328

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -1,16 +1,56 @@
-//
-//  ContentView.swift
-//  WeSplit
-//
-//  Created by Yuki Shinohara on 2020/03/28.
-//  Copyright © 2020 Yuki Shinohara. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = "" //text fieldは文字列のみ使用可
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+        Form {
+            Section {
+                TextField("Amount", text: $checkAmount)
+                .keyboardType(.decimalPad)
+                
+                Picker("Number of people", selection: $numberOfPeople) {
+                    ForEach(2 ..< 100) {
+                        Text("\($0) people")
+                    }
+                }
+            }
+            
+            Section (header: Text("How much tip do you want to leave?")){
+                
+                Picker("Tip percentage", selection: $tipPercentage) {
+                    ForEach(0 ..< tipPercentages.count) {
+                        Text("\(self.tipPercentages[$0])%")
+//                        percentage in
+//                        Text("\(self.tipPercentages[percentage])%") //同じ意味
+                    }
+                }.pickerStyle(SegmentedPickerStyle()) //選択肢少ないときはこっちが見栄えいい
+            }
+            
+          Section {
+               Text("$\(totalPerPerson, specifier: "%.2f")")
+            }
+            
+            
+        }.navigationBarTitle("WeSplit")
+    }
     }
 }
 
