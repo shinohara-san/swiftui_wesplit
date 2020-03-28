66ce60cc6400c74902d83115d8a9cad1d5fba23a
shinohara-san <someiyoshino0809@gmail.com>
2020-03-28 11:47:30 +0900

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Yuki Shinohara on 2020/03/28.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
